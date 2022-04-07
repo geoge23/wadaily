a14d6834bdd4ed09e56799e22747748360cfb8e6
George Parks <me@georgeparks.me>
2022-04-07 13:02:35 -0400

    updated acl and fixed darkmode

--- components/RadioSelector.js
+++ components/RadioSelector.js
@@ -1,7 +1,7 @@
 export default function RadioSelector({options, state, setState, className}) {
     return <div className={`flex mb-3 ${className}`}>
       {options.map(e => 
-        <p key={e} className={`mr-3 text-lg cursor-pointer ${state == e ? "font-bold text-red-900" : "text-gray-600"}`}
+        <p key={e} className={`mr-3 text-lg cursor-pointer ${state == e ? "font-bold text-red-900 dark:text-red-600" : "text-gray-600 dark:text-gray-400"}`}
             onClick={() => setState(e)}>
           {e}
         </p>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  },
 }
