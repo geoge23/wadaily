e7f4dabef46a9c8ab3837edf8cba3b81483541a3
George Parks <me@georgeparks.me>
2023-01-25 11:57:07 -0500

    added modal animation

--- components/Modal.js
+++ components/Modal.js
@@ -1,5 +1,5 @@
 export default function Modal({children, className, style}) {
-    return <div className="fixed z-50 top-0 left-0 h-screen w-screen flex justify-center items-center bg-gray-500 bg-opacity-40">
+    return <div className="modal fixed z-50 top-0 left-0 h-screen w-screen flex justify-center items-center bg-gray-500 bg-opacity-40">
         <div style={style} className={`${className} rounded-lg bg-white dark:bg-gray-900 p-5 max-w-lg flex flex-col relative`}>
             {children}
         </div>
